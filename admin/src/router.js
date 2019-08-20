ad5a52bd4ec98f7d796fe7191e8cce51c9c32fdf
董建新 <824629419@qq.com>
2019-08-20 14:18:17 +0800

    admin fix

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from './views/Main.vue'
+import CategoryEdit from './views/CategoryEdit.vue'
 
 Vue.use(Router)
 
@@ -9,8 +10,11 @@ export default new Router({
     {
       path: '/',
       name: 'main',
-      component: Main
+      component: Main,
+      children:[
+        {path:'/categories/create',component:CategoryEdit}
+      ]
     },
-    
+
   ]
 })
