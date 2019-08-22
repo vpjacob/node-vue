3ada061b5c6ae469d361e3e8ce8c740ca791b6b6
董建新 <824629419@qq.com>
2019-08-22 11:07:24 +0800

    物品 category

--- admin/src/router.js
+++ admin/src/router.js
@@ -3,6 +3,10 @@ import Router from 'vue-router'
 import Main from './views/Main.vue'
 import CategoryEdit from './views/CategoryEdit.vue'
 import CategoryList from './views/CategoryList.vue'
+
+import ItemEdit from './views/ItemEdit.vue'
+import ItemList from './views/ItemList.vue'
+
 Vue.use(Router)
 const originalPush = Router.prototype.push
 Router.prototype.push = function push(location) {
@@ -20,6 +24,11 @@ export default new Router({
         {path:'/categories/create',component:CategoryEdit},
         {path:'/categories/edit/:id',component:CategoryEdit,props:true},
         {path:'/categories/list',component:CategoryList},
+
+        {path:'/items/create',component:ItemEdit},
+        {path:'/items/edit/:id',component:ItemEdit,props:true},
+        {path:'/items/list',component:ItemList},
+
       ]
     },
 
--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="about">
-    <h1>{{id?'编辑分类':'新建分类'}}</h1>
+    <h1>{{id?'编辑':'新建'}}物品</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
-      <el-form-item label="上级分类">
-        <el-select v-model="model.parent">
-          <el-option v-for="item in parent" :key="item._id" :label='item.name' :value='item._id'></el-option>
-        </el-select>
-      </el-form-item>
+
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="图标">
+        <el-input v-model="model.icon"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -25,38 +24,33 @@ export default {
   data() {
     return {
       model: {},
-      parent: []
     };
   },
   methods: {
     async save() {
       let res
       if (this.id) {
-        res = await this.$http.put(`rest/categories/${this.id}`, this.model);
+        res = await this.$http.put(`rest/items/${this.id}`, this.model);
         this.$message({
           type: "success",
           message: "修改成功"
         });
       } else {
-        res = await this.$http.post("rest/categories", this.model);
+        res = await this.$http.post("rest/items", this.model);
         this.$message({
           type: "success",
           message: "保存成功"
         });
       }
-      this.$router.push("/categories/list");
+      this.$router.push("/items/list");
     },
     async fetch() {
-      const res = await this.$http.get(`rest/categories/${this.id}`);
+      const res = await this.$http.get(`rest/items/${this.id}`);
       this.model = res.data;
     },
-    async fetchParent() {
-      const res = await this.$http.get('rest/categories');
-      this.parent = res.data;
-    }
+    
   },
   created() {
-    this.fetchParent();
     this.id && this.fetch();
   }
 };
