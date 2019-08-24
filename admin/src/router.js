9eaf8a4bf7e3f38296067bbde166194b9be046a4
董建新 <824629419@qq.com>
2019-08-24 15:42:52 +0800

    admin page

--- admin/src/router.js
+++ admin/src/router.js
@@ -16,6 +16,10 @@ import ArticleList from './views/ArticleList.vue'
 import AdEdit from './views/AdEdit.vue'
 import AdList from './views/AdList.vue'
 
+import AdminUserEdit from './views/AdminUserEdit.vue'
+import AdminUserList from './views/AdminUserList.vue'
+
+
 Vue.use(Router)
 const originalPush = Router.prototype.push
 Router.prototype.push = function push(location) {
@@ -49,6 +53,10 @@ export default new Router({
         {path:'/ads/create',component:AdEdit},
         {path:'/ads/edit/:id',component:AdEdit,props:true},
         {path:'/ads/list',component:AdList},
+        
+        {path:'/admin_users/create',component:AdminUserEdit},
+        {path:'/admin_users/edit/:id',component:AdminUserEdit,props:true},
+        {path:'/admin_users/list',component:AdminUserList},
 
       ]
     },
